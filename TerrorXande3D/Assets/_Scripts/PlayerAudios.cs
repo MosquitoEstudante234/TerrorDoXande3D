a6085b62163082f46bf992348f786ed1e7164e15
MosquitoEstudante234 <mosquitoestudante34234@gmail.com>
2024-08-14 08:52:21 -0300

    Footsteps player

--- TerrorXande3D/Assets/_Scripts/PlayerAudios.cs
+++ TerrorXande3D/Assets/_Scripts/PlayerAudios.cs
@@ -2,20 +2,30 @@ using UnityEngine;
 
 public class PlayerAudios : MonoBehaviour
 {
+    public AudioSource Footsteps;
     public GameObject[] playerAudios = new GameObject[0];
+    public float normalPitch = 1f;
+    public float runningPitch = 2f;
+
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.D))
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D))
         {
             playerAudios[0].SetActive(true);
+
+            if (Input.GetKey(KeyCode.LeftShift))
+            {
+                Footsteps.pitch = runningPitch;
+            }
+            else
+            {
+                Footsteps.pitch = normalPitch;
+            }
         }
         else
         {
             playerAudios[0].SetActive(false);
-        }
-        if (Input.GetKeyDown(KeyCode.LeftShift))
-        {
-
+            Footsteps.pitch = normalPitch; 
         }
     }
 }
